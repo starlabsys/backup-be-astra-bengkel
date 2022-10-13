fffacf284a29196aee13f7bd17d04c144f1bac59
Medianto Pratama Pentadakosta <pentadakosta99@student.untan.ac.id>
2022-10-13 12:28:47 +0700

    setup deploy

--- src/config/database.js
+++ src/config/database.js
@@ -16,10 +16,12 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
+    username: "doadmin",
+    password: "AVNS_Gj2hiA0Z6AeLpgJ_7wb",
+    database: "db-astra",
+    host: "db-ta-mysql-sgp1-88822-do-user-12614603-0.b.db.ondigitalocean.com",
     dialect: "mysql",
+    port: 25060,
+    ssl: true
   },
 };
