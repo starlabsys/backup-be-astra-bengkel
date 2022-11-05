3df21d73176e94403901dba7d0e154b4213d4a34
febryanpratama <spandupan46@student.untan.ac.id>
2022-11-06 01:59:26 +0700

    feat: rev part

--- src/Validators/PartsValidator.ts
+++ src/Validators/PartsValidator.ts
@@ -3,8 +3,8 @@ import { check, validationResult } from "express-validator";
 const db = require("../db/models");
 
 const validate = [
-    check("parts_code", "Please enter a parts code").isLength({ min: 6 }),
-    check("parts_name", "Please enter a Parts Name").isLength({ min: 6 }),
+    check("parts_code", "Please enter a parts code").isLength({ min: 3 }),
+    check("parts_name", "Please enter a Parts Name").isLength({ min: 3 }),
     check("parts_qty", "Please enter a Parts Qty").isNumeric().isLength({min:1}),
     check("parts_price", "Please enter a Parts Price").isNumeric().isLength({min:1}),
     (req: Request, res: Response, next: NextFunction) => {
