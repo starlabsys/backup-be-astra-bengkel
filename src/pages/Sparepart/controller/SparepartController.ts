94b8990a0f325c0030d516e7539acad74bc28d38
febryanpratama <spandupan46@student.untan.ac.id>
2022-12-28 17:04:08 +0700

    feat: detail sparepart

--- src/pages/Sparepart/controller/SparepartController.ts
+++ src/pages/Sparepart/controller/SparepartController.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 import SparepartRepository from "../../../domain/repository/SparepartRepository/SparepartRepository";
 import Token from "../../../utils/Token";
 import { EnumResponseCode } from "../../../utils/enum/EnumResponseCode";
+import { InterfaceGetDetailSparepart } from "../../../domain/repository/SparepartRepository/interface/InterfaceGetDetailSparepart";
 
 
 class SparepartController {
@@ -116,6 +117,33 @@ class SparepartController {
                 data : e
             } )
         }
+    }
+
+    public detailSparepart = async ( req : Request, res : Response ) : Promise<Response> => {
+        const data: InterfaceGetDetailSparepart = req.body;
+        const token = await Token.get( req, res );
+
+        try{
+            const resp = await SparepartRepository.getDetailSparepart( res, token ?? '', data );
+            if(resp !== null){
+                return ResponseResult.successGet(res, resp);
+            }
+
+            return ResponseResult.error(res, {
+                statusCode: EnumResponseCode.INTERNAL_SERVER_ERROR,
+                errorCode: '01',
+                message: "Internal Error",
+                data: null
+            })
+        }catch(e:any){
+            return ResponseResult.error(res, {
+                statusCode: 400,
+                errorCode: '01',
+                message: e.message,
+                data: e
+            })
+        }
+
     }
 }
 
