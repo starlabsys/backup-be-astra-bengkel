a3153864dd7bdc639bd281880ee44387f9e12169
Medianto Pratama Pentadakosta <pentadakosta99@student.untan.ac.id>
2023-01-01 13:46:06 +0700

    vendor

--- src/domain/repository/VendorRepository/VendorRepository.ts
+++ src/domain/repository/VendorRepository/VendorRepository.ts
@@ -2,8 +2,11 @@ import { Response } from "express";
 import { post } from "../../../core/api/api";
 import { InterfaceGetVendor } from "./interface/InterfaceGetVendor";
 import { ConvertGetVendor } from "../../models/Vendor/ModelGetVendor";
-// import { ConvertModelGetTipeKendaraan, ModelGetTipeKendaraan } from "../../models/TipeKendaraan/ModelGetTipeKendaraan";
+// import { ConvertModelGetTipeKendaraan, ModelGetTipeKendaraan } from
+// "../../models/TipeKendaraan/ModelGetTipeKendaraan";
 import { ModelGetVendor } from "../../models/Vendor/ModelGetVendor";
+import { InterfaceAddVendor } from "./interface/InterfaceAddVendor";
+import { ConvertModelResult, ModelResult } from "../../models/Result/ModelResult";
 
 
 class VendorRepository {
@@ -19,17 +22,22 @@ class VendorRepository {
         return null;
     }
 
-    // public editData = async ( res : Response, token : string, reqBody : InterfaceEditTipeKendaraan ) : Promise<ModelResult | null> => {
-    //     const resp = await post( res, {
-    //         url : '/api/Master/PUTTipeKendaraan',
-    //         token : token,
-    //         reqBody : reqBody
-    //     } )
-    //     if ( resp !== null ) {
-    //         return ConvertModelResult.toModelResult( resp );
-    //     }
-    //     return null;
-    // }
+    public addVendor = async ( res : Response, token : string, reqBody : InterfaceAddVendor ) : Promise<ModelResult | null> => {
+        const resp = await post( res, {
+            url : '/api/Master/PUTVendor',
+            token : token,
+            reqBody : reqBody
+        } )
+        if ( resp !== null ) {
+            return ConvertModelResult.toModelResult( resp );
+        }
+        return null;
+    }
+
+    // public editData = async ( res : Response, token : string, reqBody : InterfaceEditTipeKendaraan ) :
+    // Promise<ModelResult | null> => { const resp = await post( res, { url : '/api/Master/PUTTipeKendaraan', token :
+    // token, reqBody : reqBody } ) if ( resp !== null ) { return ConvertModelResult.toModelResult( resp ); } return
+    // null; }
 
 }
 
