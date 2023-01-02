0948d856cb5ed5c72f0e6cba91a5625381df72e9
febryanpratama <spandupan46@student.untan.ac.id>
2023-01-02 21:29:10 +0700

    feat: rev pit admin

--- src/pages/Pit/routes/PitRoutes.ts
+++ src/pages/Pit/routes/PitRoutes.ts
@@ -1,11 +1,11 @@
 import BaseRoutes from "../../../core/routes/BaseRoutes";
-import { authAdmin } from "../../../middleware/AdminMiddleware";
+import { authAdmin, authAll } from "../../../middleware/AdminMiddleware";
 import PitController from "../controller/PitController";
 
 
 class PitRoutes extends BaseRoutes {
     routes() : void {
-        this.router.post( '/get', authAdmin, PitController.getPit )
+        this.router.post( '/get', authAll, PitController.getPit )
         this.router.post('/store', authAdmin, PitController.storePit )
         // this.router.post( '/edit', authAdmin, VendorRepository.editData )
     }
