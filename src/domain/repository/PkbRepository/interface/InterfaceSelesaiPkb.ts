c97b644ce6905dee50f1531bdd3b24811d0e57b5
febryanpratama <spandupan46@student.untan.ac.id>
2023-01-12 22:56:42 +0700

    feat laporan

---
+++ src/domain/repository/PkbRepository/interface/InterfaceSelesaiPkb.ts
@@ -0,0 +1,20 @@
+export interface InterfaceSelesaiPkb {
+  id: number,
+  action: number,
+  waktu: string,
+  refMechanicId: 2,
+  saran: string,
+  durasiPengerjaanPKB: string,
+  isOverdue: string,
+  etaOverdue: string,
+  alasanPauseId: string
+}
+// id: 393453,
+// action: 2,
+// waktu: "2023-01-12T13:51:14+07:00",
+// refMechanicId: 2,
+// saran: "",
+// durasiPengerjaanPKB: "00:00:24:41",
+// isOverdue: "",
+// etaOverdue: "",
+// alasanPauseId: ""
