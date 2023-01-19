e8988909c030d372f0bd4566dbc8c5fefda196cc
Medianto Pratama Pentadakosta <pentadakosta99@student.untan.ac.id>
2023-01-19 11:53:13 +0700

    controller data pkb

--- src/pages/Pkb/controller/PkbImportController.ts
+++ src/pages/Pkb/controller/PkbImportController.ts
@@ -403,14 +403,14 @@ class PkbImportController {
                     // return ResponseResult.successGet( res, "ada" )
                     const responseKendaraan = await GetData.getKendaraan( req, res, {
                         token : token ?? '',
-                        no_polisi : element.no_polisi,
-                        no_mesin : element.no_mesin,
-                        warna : element.warna,
-                        no_rangka : element.no_rangka,
-                        nama_tipe_kendaraan : element.nama_tipe_kendaraan,
-                        idPelangan : checkCustomer?.listPelanggan[ 0 ].id ?? 0,
-                        idPelanganSTNK : checkCustomer?.listPelanggan[ 0 ].id ?? 0,
-                        tahunRakit : element.tahun_rakit,
+                        no_polisi : "",//element.no_polisi
+                        no_mesin : element.no_mesin,//
+                        warna : "",//element.warna
+                        no_rangka : "",//element.no_rangka
+                        nama_tipe_kendaraan : "",//element.nama_tipe_kendaraan
+                        idPelangan : 0,//checkCustomer?.listPelanggan[ 0 ].id ?? 0
+                        idPelanganSTNK : 0,//checkCustomer?.listPelanggan[ 0 ].id ?? 0
+                        tahunRakit : 0,//element.tahun_rakit
                     } )
                     console.log( "ada" )
 
