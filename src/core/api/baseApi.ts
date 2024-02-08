a0e7b1bc38456c3ac1911cd1414c7048deb0e440
QyraPentadakosta99 <142347562+mp-pentadakosta@users.noreply.github.com>
2024-02-09 01:52:28 +0700

    change base url

--- src/core/api/baseApi.ts
+++ src/core/api/baseApi.ts
@@ -5,10 +5,11 @@ import ModelUsers from "../../db/models/ModelUsers";
 export const timeOut = 50000
 
 export const baseUrl = () : string => {
-    if ( process.env.ENV === 'production' ) {
-        return process.env.BASE_URL_PROD as string
-    }
-    return process.env.BASE_URL_DEV as string
+    return "https://psshsoapi.astra.co.id/ahasssystemapi"
+    // if ( process.env.ENV === 'production' ) {
+    //     return process.env.BASE_URL_PROD as string
+    // }
+    // return process.env.BASE_URL_DEV as string
 }
 
 interface InterfaceHeader {
