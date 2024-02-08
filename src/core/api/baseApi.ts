bd062b0b2067fd4d4cda45fbd69886d1a0ccf898
QyraPentadakosta99 <142347562+mp-pentadakosta@users.noreply.github.com>
2024-02-09 01:46:58 +0700

    change base user production

--- src/core/api/baseApi.ts
+++ src/core/api/baseApi.ts
@@ -5,7 +5,7 @@ import ModelUsers from "../../db/models/ModelUsers";
 export const timeOut = 50000
 
 export const baseUrl = () : string => {
-    if ( process.env.ENV === 'prod' ) {
+    if ( process.env.ENV === 'production' ) {
         return process.env.BASE_URL_PROD as string
     }
     return process.env.BASE_URL_DEV as string
