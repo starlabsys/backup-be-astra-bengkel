dfb2df8cf517b17e4ce26a0c77b74dbec201963c
Febryan Pratama <febryancaesarpratama@gmail.com>
2024-07-08 15:10:43 +0700

    feat: adding user-agent

--- src/core/api/baseApi.ts
+++ src/core/api/baseApi.ts
@@ -21,6 +21,7 @@ export const header = async ( props : InterfaceHeader ) => {
         "Content-Type" : props.header ? "text/plain" : "application/json",
         "Accept" : "application/json, text/plain, */*",
         "Origin" : "https://psshsoapi.astra.co.id",
+        'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
         'Authorization' : `Bearer ${ props.token }`
     }
 }
